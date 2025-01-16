1a744b1e7aefcdb69b9951978a132cdef96667ef
ChrisSouthey <chsouthey@email.neit.edu>
2025-01-16 10:12:55 -0500

    Sart

---
+++ includes/style.php
@@ -0,0 +1,92 @@
+<style>
+@charset "UTF-8";
+
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+}
+
+body{
+    width:1920px;
+    height: 1080px;
+    background-color: #f3f4f5;
+}
+
+/* ---------------- Header Section --------- */
+.logo{
+    width:500px;
+    height:280px;
+    background-color: #BF0000;
+    border-radius: 0px 0px 60px 0px;
+}
+
+header{
+    width:1420px;
+    height:160px;
+    background-color: #BF0000;
+}
+
+.flex{
+    display:flex;
+}
+
+.search{
+    width:820px;
+    height:121px;
+    background-color:#242024;
+    border-radius: 0px 0px 30px 30px;
+}
+
+.cardinfo{
+    width:600px;
+    height:875px;
+    background-color:#DDDDDD;
+}
+
+.sc{
+    display:flex;
+}
+
+/* ---------------- Filter Section --------- */
+.filter{
+   width:370px;
+   height:755px;
+   background-color:#DDDDDD; 
+   border-radius: 0px 30px 0px 0px;
+}
+
+/* ---------------- Body Section --------- */
+.fm{
+    display:flex;
+}
+
+.mainbox{
+    width:950px;
+    height:755px;
+    background:#DDDDDD;
+}
+
+
+/* ---------------- Footer Section --------- */
+footer{
+    width:1920px;
+    height:45px;
+    background: #BF0000;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</style>
--- index.php
+++ index.php
@@ -1,10 +1,37 @@
-<?php include 'includes/header.php '; ?>
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/style.php'; ?>
 
-<style>
-    body{
-        background-color: red
-         }
-</style>
+<div class="flex"> <!-- MAKES EVERYTHING FLEX -->
+    <div class="lf">
+        <section class="logo"> <!-- LOGO BOX -->
 
+        </section>
+        <div class="fm">
+            <section class="filter"> <!-- FILTER BOX -->
 
+            </section>
+            <section class="mainbox">
+
+            </section>
+        </div>
+    </div>
+    
+    <div>
+        <header>
+
+        </header>
+        <div class="sc">
+            <section class="search"> <!-- SEARCH BOX-->
+
+            </section>
+            <section class="cardinfo"> <!-- CARD INFO BOX -->
+
+            </section>
+        </div>
+    </div>
+</div>
+
+<footer> <!-- FOOTER -->
+
+</footer>
 <?php include 'includes/footer.php'; ?>
