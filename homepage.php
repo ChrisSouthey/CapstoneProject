b2d0f1ee3c4b931fa8483060729d91dec3c0921a
curry7455 <156011654+curry7455@users.noreply.github.com>
2025-02-16 20:18:15 -0500

    filterbox test
    filter box

--- homepage.php
+++ homepage.php
@@ -51,11 +51,63 @@ $userID = $_SESSION['user']['id'];
         </div>
     </div>
     <div class="left">
-        <div class="filter">
-            <div class="filtText">
-                <p>Filter</p>
+    <div class="filter">
+    <div class="filtText">
+        <p>My Collection</p>
+    </div>
+    <div class="dropdown-container">
+        <div class="dropdown">
+            <div class="dropdown-header" onclick="toggleDropdown('game-name')">
+                &#9654; Game Name
+            </div>
+            <div class="dropdown-content" id="game-name">
+                <label><input type="checkbox"> Pokemon</label>
+                <label><input type="checkbox"> Magic the Gathering</label>
+                <label><input type="checkbox"> One Piece</label>
+                <label><input type="checkbox"> Game 3</label>
+                <label><input type="checkbox"> Game 4</label>
+                <label><input type="checkbox"> Game 5</label>
             </div>
         </div>
+        <div class="dropdown">
+            <div class="dropdown-header" onclick="toggleDropdown('card-type')">
+                &#9654; Card Type
+            </div>
+            <div class="dropdown-content" id="card-type">
+                <label><input type="checkbox"> Type A</label>
+                <label><input type="checkbox"> Type B</label>
+                <label><input type="checkbox"> Type C</label>
+                <label><input type="checkbox"> Type D</label>
+                <label><input type="checkbox"> Type E</label>
+            </div>
+        </div>
+        <div class="dropdown">
+            <div class="dropdown-header" onclick="toggleDropdown('card-color')">
+                &#9654; Card Color
+            </div>
+            <div class="dropdown-content" id="card-color">
+                <label><input type="checkbox"> Color A</label>
+                <label><input type="checkbox"> Color B</label>
+                <label><input type="checkbox"> Color C</label>
+                <label><input type="checkbox"> Color D</label>
+                <label><input type="checkbox"> Color E</label>
+            </div>
+        </div>
+        <div class="dropdown">
+            <div class="dropdown-header" onclick="toggleDropdown('card-rarity')">
+                &#9654; Card Rarity
+            </div>
+            <div class="dropdown-content" id="card-rarity">
+                <label><input type="checkbox"> Rarity A</label>
+                <label><input type="checkbox"> Rarity B</label>
+                <label><input type="checkbox"> Rarity C</label>
+                <label><input type="checkbox"> Rarity D</label>
+                <label><input type="checkbox"> Rarity E</label>
+            </div>
+        </div>
+    </div>
+</div>
+
         <div  class="accsettings">
             <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#ffffff"><path d="M222-255q63-44 125-67.5T480-346q71 0 133.5 23.5T739-255q44-54 62.5-109T820-480q0-145-97.5-242.5T480-820q-145 0-242.5 97.5T140-480q0 61 19 116t63 109Zm257.81-195q-57.81 0-97.31-39.69-39.5-39.68-39.5-97.5 0-57.81 39.69-97.31 39.68-39.5 97.5-39.5 57.81 0 97.31 39.69 39.5 39.68 39.5 97.5 0 57.81-39.69 97.31-39.68 39.5-97.5 39.5Zm.66 370Q398-80 325-111.5t-127.5-86q-54.5-54.5-86-127.27Q80-397.53 80-480.27 80-563 111.5-635.5q31.5-72.5 86-127t127.27-86q72.76-31.5 155.5-31.5 82.73 0 155.23 31.5 72.5 31.5 127 86t86 127.03q31.5 72.53 31.5 155T848.5-325q-31.5 73-86 127.5t-127.03 86Q562.94-80 480.47-80Zm-.47-60q55 0 107.5-16T691-212q-51-36-104-55t-107-19q-54 0-107 19t-104 55q51 40 103.5 56T480-140Zm0-370q34 0 55.5-21.5T557-587q0-34-21.5-55.5T480-664q-34 0-55.5 21.5T403-587q0 34 21.5 55.5T480-510Zm0-77Zm0 374Z"/></svg><a href="accountPages/settings.php?ID=<?= $userID ?>" class="userName"><?= $_SESSION['name']; ?></a>
         </div>
@@ -69,6 +121,35 @@ $userID = $_SESSION['user']['id'];
     
 
 </div>
+<style>
+    .dropdown-container {
+        width: 200px;
+        background: #333;
+        padding: 10px;
+        border-radius: 5px;
+    }
+    .dropdown {
+        margin-bottom: 10px;
+    }
+    .dropdown-header {
+        background: #444;
+        padding: 8px;
+        cursor: pointer;
+        color: white;
+        border-radius: 3px;
+    }
+    .dropdown-content {
+        display: none;
+        padding: 5px;
+        background: #555;
+        border-radius: 3px;
+    }
+    .dropdown-content label {
+        display: block;
+        color: white;
+        padding: 2px;
+    }
+</style>
 <script>
     var gameSel = document.getElementById("game");
     var opt = document.getElementById("opt");
@@ -82,6 +163,18 @@ $userID = $_SESSION['user']['id'];
         console.log(game);
         window.location = "homepage.php?game=" + game
     })
+
+    function toggleDropdown(id) {
+        var content = document.getElementById(id);
+        var header = content.previousElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+            header.innerHTML = "&#9654; " + header.innerHTML.slice(2);
+        } else {
+            content.style.display = "block";
+            header.innerHTML = "&#9660; " + header.innerHTML.slice(2);
+        }
+    }
     
     
     
